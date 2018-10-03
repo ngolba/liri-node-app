643fcde8ee59624055a19bbd3882768b54bb6c78
Nathan Golba <ngolba@gmail.com>
2018-10-03 04:02:23 -0400

    changes

--- liri.js
+++ liri.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 let keys = require("./keys");
+console.log(keys.spotify)
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
 
 let firstInput = process.argv[2];
 
@@ -9,7 +10,8 @@ let firstInput = process.argv[2];
 if (firstInput === "concert-this") {
     let artist = '';
     for (let i = 3; i < process.argv.length; i++){
-        artist = artist.concat(process.argv[i])
+        artist = artist.concat(process.argv[i] + ' ')
     }
-    console.log(artist);
+    let queryUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`
+    console.log(queryUrl)
 }
