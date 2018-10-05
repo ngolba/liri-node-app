91cbb12965b16ae1c7344542d0eca0d95976dd7b
Nathan Golba <ngolba@gmail.com>
2018-10-05 19:25:40 -0400

    Working Model

--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+/// Globals  /////////
 require("dotenv").config();
 const fs = require('fs');
 const keys = require("./keys");
@@ -5,51 +6,67 @@ const moment = require('moment');
 const request = require('request');
 const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);
-
-const command = process.argv[2];
+let command = process.argv[2];
 const acceptableCommands = ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"];
-if (!acceptableCommands.includes(command)) {
-    console.log(`Command not recognized. Acceptable Commands are: \n${acceptableCommands[0]} \n${acceptableCommands[1]} \n${acceptableCommands[2]} \n${acceptableCommands[3]}`)
+//////////////////////////////////////
+
+// Log initial input to log.txt ////
+let cmdLineText = '';
+for (let i = 2; i < process.argv.length; i++){
+    cmdLineText += `${process.argv[i]} `
 }
+fs.appendFile('log.txt', `${cmdLineText}\n\n`, 'utf8', () => {});
+///////////////////////////////
 
-const processInput = (divider, input) => {
-    let outputString = input[3];
+// Console.log data and store to log.txt //
+const logAndPrintData = (data) => {
+    console.log(data)
+    fs.appendFile('log.txt', (data +  '\n'), 'utf8', () => {});
+}
+/////////////////////////////////////////
 
-    if (input.length === 4) {
+// Format input for API use ///////
+const processInput = (divider, input, minLength) => {
+    let outputString = input[minLength];
+    if (input.length === (minLength + 1)) {
         return outputString;
     } else {
-        for (let i = 4; i < input.length; i++) {
+        for (let i = (minLength + 1); i < input.length; i++) {
             outputString += (divider + input[i]);
         }
         return outputString;
     }
 }
+/////////////////////////////////
 
-if (command === "concert-this") {
-    let artist = processInput('%20', process.argv);
+// Bands in Town API ///////
+const concertThis = (input, minLength) => {
+    let artist = processInput('%20', input, minLength);
     let queryUrl = `https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`
 
     request(queryUrl, function (error, response, body) {
         if (error) {
-            console.log("Error. Please try again")
+            logAndPrintData("Error. Please try again.\n")
             return;
         }
 
         let bandInfo = JSON.parse(body);
         if (!bandInfo[0]) {
-            console.log("No upcoming Events")
+            logAndPrintData("No upcoming Events\n")
             return;
         }
 
         let event = {};
         for (let i = 0; i < bandInfo.length; i++) {
             event = bandInfo[i]
-            console.log(`Venue: ${event.venue.name}`)
-            console.log(`Location: ${event.venue.city}, ${event.venue.region}`)
-            console.log(`Date: ${moment(event.datetime).format("MM/DD/YYYY")} \n`)
+            logAndPrintData(`Venue: ${event.venue.name} \nLocation: ${event.venue.city}, ${event.venue.region}  \nDate: ${moment(event.datetime).format("MM/DD/YYYY")} \n`)
         }
     });
-} else if (command === "spotify-this-song") {
+}
+//////////////////////////////
+
+// Spotify through node-spotify-api 
+const spotifyThis = (input, minLength) => {
     let songName = '';
     let artists = '';
     let previewUrl = '';
@@ -68,17 +85,16 @@ if (command === "concert-this") {
         previewUrl = data.preview_url;
         album = data.album.name;
     }
-    const printData = () => console.log(`Song Name: ${songName} \nArtist(s): ${artists} \nPreview URL: ${previewUrl} \nAlbum Title: ${album}`)
 
-    if (!process.argv[3]) {
+    if (!input[minLength]) {
         spotify
             .request("https://api.spotify.com/v1/tracks/3DYVWvPh3kGwPasp7yjahc")
             .then((data) => {
                 fillData(data);
-                printData();
+                logAndPrintData(`Song Name: ${songName} \nArtist(s): ${artists} \nPreview URL: ${previewUrl} \nAlbum Title: ${album} \n`);
             })
     } else {
-        songName = processInput(' ', process.argv)
+        songName = processInput(' ', input, minLength)
         spotify
             .search({
                 type: 'track',
@@ -87,35 +103,61 @@ if (command === "concert-this") {
             })
             .then(function (response) {
                 fillData(response.tracks.items[0]);
-                printData();
+                logAndPrintData(`Song Name: ${songName} \nArtist(s): ${artists} \nPreview URL: ${previewUrl} \nAlbum Title: ${album} \n`);
             })
     }
+}
+///////////////////////////////////
 
-
-
-} else if (command === "movie-this") {
+// OMDb API ///////////////////////
+const movieThis = (input, minLength) => {
     let movieQuery = '';
-    if (!process.argv[3]){
+    if (!input[minLength]) {
         movieQuery = `http://www.omdbapi.com/?apikey=trilogy&i=tt0485947`
     } else {
-        movieQuery = `http://www.omdbapi.com/?apikey=trilogy&t=${processInput('+', process.argv)}`
+        movieQuery = `http://www.omdbapi.com/?apikey=trilogy&t=${processInput('+', input, minLength)}`
     }
-    
+
     request(movieQuery, (error, response, body) => {
         if (error) {
-            console.log("Error. Please try again")
+            logAndPrintData("Error. Please try again.\n")
             return;
         }
         let movieInfo = JSON.parse(body);
-        console.log(`Title: ${movieInfo.Title}`)
-        console.log(`Year: ${movieInfo.Year}`)
-        console.log(`IMDb Rating: ${movieInfo.imdbRating}`)
-        console.log(`Rotten Tomatoes Rating: ${movieInfo.Ratings[1].Value}`)
-        console.log(`Country: ${movieInfo.Country}`)
-        console.log(`Language: ${movieInfo.Language}`)
-        console.log(`Plot: ${movieInfo.Plot}`)
-        console.log(`Actors: ${movieInfo.Actors}`)
+        logAndPrintData(`Title: ${movieInfo.Title} \nYear: ${movieInfo.Year} \nIMDb Rating: ${movieInfo.imdbRating} \nRotten Tomatoes Rating: ${movieInfo.Ratings[1].Value} \nCountry: ${movieInfo.Country} \nLanguage: ${movieInfo.Language} \nPlot: ${movieInfo.Plot} \nActors: ${movieInfo.Actors}\n`)
     })
-}else if (command === "do-what-it-says") {
-    
+}
+//////////////////////////////////
+
+
+if (command === "concert-this") {
+    concertThis(process.argv, 3);
+} else if (command === "spotify-this-song") {
+    spotifyThis(process.argv, 3);
+} else if (command === "movie-this") {
+    movieThis(process.argv, 3)
+} else if (command === "do-what-it-says") {
+    fs.readFile('random.txt', 'utf-8', (err, data) => {
+        if (err) {
+            console.log("Error")
+            return;
+        }
+        
+        let fileText = (JSON.stringify(data)).replace(/["'\\]/g, '');
+        let fileInputs = fileText.split(',');
+        command = fileInputs[0];
+        let itemName = fileInputs[1].split(' ');
+
+        if (command === "concert-this") {
+            concertThis(itemName, 0);
+        } else if (command === "spotify-this-song") {
+            spotifyThis(itemName, 0);
+        } else if (command === "movie-this") {
+            movieThis(itemName, 0)
+        } else {
+            console.log("Command not recognized.")
+        }
+    })
+} else {
+    console.log(`Command not recognized. Acceptable Commands are: \n${acceptableCommands[0]} \n${acceptableCommands[1]} \n${acceptableCommands[2]} \n${acceptableCommands[3]}`)
 }
